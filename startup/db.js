8ccafba512cc703f13d9d991235d3c14cc5f5c56
Maibritt Rittermann <mri@skivecollege.dk>
2022-03-31 12:23:49 +0200

    test

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const debug = require("debug")("app:db");
 
 module.exports = function () {
+  console.log(config.get("db"));
   mongoose
     .connect(config.get("db"))
     .then(() => debug("Connected to db"))
