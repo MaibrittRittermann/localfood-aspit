2e97a3946de9a71d271212b121e5378fd3730fa4
Maibritt <mri@skivecollege.dk>
2022-04-18 12:46:40 +0200

    Updated test to seller

--- tests/integration/routes/seller.test.js
+++ tests/integration/routes/seller.test.js
@@ -1,31 +1,31 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
-const { Farmer } = require("../../../model/farmer");
+const { Seller } = require("../../../model/seller");
 const { User } = require("../../../model/user");
 
 let server;
 
-describe("/api/farmer", () => {
+describe("/api/seller", () => {
   beforeEach(() => {
     server = require("../../../index");
   });
 
   afterEach(async () => {
-    await Farmer.deleteMany({});
+    await Seller.deleteMany({});
     await server.close();
   });
 
   describe("GET /", () => {
-    it("should return all farmers", async () => {
+    it("should return all sellers", async () => {
       try {
-        await Farmer.collection.insertMany([
+        await Seller.collection.insertMany([
           { name: "aaa", address: "aaa", zip: 1001, city: "aaa" },
           { name: "bbb", address: "bbb", zip: 1001, city: "bbb" },
         ]);
       } catch (e) {
         console.log(e);
       }
-      const res = await request(server).get("/api/farmer");
+      const res = await request(server).get("/api/seller");
       expect(res.status).toBe(200);
       expect(res.body.some((f) => f.name === "aaa")).toBeTruthy();
     });
@@ -33,29 +33,29 @@ describe("/api/farmer", () => {
 });
 
 describe("GET /:id", () => {
-  it("should return the farmer with the given id", async () => {
-    const farmer = new Farmer({
+  it("should return the seller with the given id", async () => {
+    const seller = new Seller({
       name: "aaa",
       address: "aaa",
       zip: 1001,
       city: "aaa",
     });
-    farmer.save();
+    seller.save();
 
-    const res = await request(server).get("/api/farmer/" + farmer._id);
+    const res = await request(server).get("/api/seller/" + seller._id);
 
     expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty("name", farmer.name);
+    expect(res.body).toHaveProperty("name", seller.name);
   });
 
-  it("should return 404 when given an invalid farmer ID", async () => {
-    const res = await request(server).get("/api/farmer/1");
+  it("should return 404 when given an invalid seller ID", async () => {
+    const res = await request(server).get("/api/seller/1");
     expect(res.status).toBe(404);
   });
 
-  it("should return 404 when given an invalid but nonexisting farmer ID", async () => {
+  it("should return 404 when given an invalid but nonexisting seller ID", async () => {
     const res = await request(server).get(
-      "/api/farmer/" + new mongoose.Types.ObjectId()
+      "/api/seller/" + new mongoose.Types.ObjectId()
     );
     expect(res.status).toBe(404);
   });
@@ -63,11 +63,11 @@ describe("GET /:id", () => {
 
 describe("POST /", () => {
   let token;
-  let farmer;
+  let seller;
 
   beforeEach(() => {
     token = new User().generateAuthToken();
-    farmer = {
+    seller = {
       name: "aaa",
       address: "aaa",
       zip: 1001,
@@ -81,39 +81,39 @@ describe("POST /", () => {
 
   it("should return 401 if the user is not logged in", async () => {
     token = "";
-    const res = await request(server).post("/api/farmer").send(farmer);
+    const res = await request(server).post("/api/seller").send(seller);
     expect(res.status).toBe(401);
   });
 
   it("should return 400 if name is less than 3 characters", async () => {
-    farmer.name = "a";
+    seller.name = "a";
     const res = await request(server)
-      .post("/api/farmer")
+      .post("/api/seller")
       .set("x-auth-token", token)
-      .send(farmer);
+      .send(seller);
     expect(res.status).toBe(400);
   });
 
   if (
     ("should return 400 if name is more than 255",
     async () => {
-      farmer.name = new Array(256).join("a");
+      seller.name = new Array(256).join("a");
       // todo rest
     })
   );
 
   if (
-    ("should save and return farmer if valid",
+    ("should save and return seller if valid",
     async () => {
       const res = await request(server)
-        .post("/api/farmer")
+        .post("/api/seller")
         .set("x-auth-token", token)
-        .send(farmer);
+        .send(seller);
 
       expect(res.status).toBe(200);
-      expect(res.body).toHaveProperty("name", farmer.name);
+      expect(res.body).toHaveProperty("name", seller.name);
 
-      const resultat = Farmer.find({ _id: farmer._id });
+      const resultat = Seller.find({ _id: seller._id });
       expect(resultat).not.toBeNull();
     })
   );
